269b1b97cdb5c71ee6b6807ec5c73db6fb78b2fb
Mahmudul-hasan700 <128229930+Mahmudul-hasan700@users.noreply.github.com>
2023-12-13 07:10:22 +0000

    A

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,7 +1,7 @@
 // src/components/BlogDetails.jsx
-import React from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { getBlogById, getAuthors, getBlogs } from '../blogData.js';
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+import { getBlogById, getAuthors, getBlogs } from "../blogData.js";
 
 const BlogDetails = () => {
   const { id } = useParams();
@@ -60,44 +60,103 @@ const BlogDetails = () => {
         {/* Displaying author information */}
         <div className="flex items-center mb-4">
           <img
-            src={getAuthors().find((author) => author.id === blog.authorId)?.image}
-            alt={getAuthors().find((author) => author.id === blog.authorId)?.Name}
+            src={
+              getAuthors().find((author) => author.id === blog.authorId)?.image
+            }
+            alt={
+              getAuthors().find((author) => author.id === blog.authorId)?.Name
+            }
             className="w-8 h-8 rounded-full mr-2"
           />
           <p className="text-sm text-gray-500">
-            {getAuthors().find((author) => author.id === blog.authorId)?.Name} | {blog.createdDate}
+            {getAuthors().find((author) => author.id === blog.authorId)?.Name} |{" "}
+            {blog.createdDate}
           </p>
         </div>
 
         {/* Displaying HTML content */}
-        <div className="text-[15px]" dangerouslySetInnerHTML={{ __html: blog.content }} />
+        <div
+          className="text-[15px]"
+          dangerouslySetInnerHTML={{ __html: blog.content }}
+        />
       </div>
 
       <div className="mt-8 flex items-center justify-center w-full">
         <Link to="/">
           <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-4 rounded flex items-center justify-center gap-2">
-            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
-            </svg> View all posts
+            <svg
+              class="w-6 h-6 text-white"
+              ariahidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokelinecap="round"
+                strokelinejoin="round"
+                strokewidth="2"
+                d="M13 5H1m0 0 4 4M1 5l4-4"
+              />
+            </svg>{" "}
+            View all posts
           </button>
         </Link>
       </div>
 
       {/* Related Articles Section */}
       <div className="mt-8 font-inter">
-        <h2 className="text-2xl font-semibold mb-4 text-center font-inter">You May Also Like</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-center font-inter">
+          You May Also Like
+        </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {relatedBlogs.map((relatedBlog) => (
-            <div key={relatedBlog.id} className="bg-white p-4 rounded-lg shadow-md">
+            <div
+              key={relatedBlog.id}
+              className="bg-white p-4 rounded-lg shadow-md"
+            >
               <Link to={`/blog/${relatedBlog.id}`}>
                 <img
                   src={relatedBlog.image}
                   alt={relatedBlog.title}
                   className="mb-2 rounded-md h-32 w-full object-cover"
                 />
-                <p className="text-lg font-semibold text-gray-900 hover:underline">
+                <p className="text-lg font-semibold text-gray-900 hover:underline line-clamp-3 text-ellipsis">
                   {relatedBlog.title}
                 </p>
+                {/* Displaying categories */}
+                <div className="flex items-center mb-2">
+                  {blog.categories.map((category, index) => (
+                    <span
+                      key={index}
+                      className="text-sm text-indigo-500 bg-indigo-100 px-2 py-1 rounded-full mr-2"
+                    >
+                      {category}
+                    </span>
+                  ))}
+                </div>
+
+                {/* Displaying author information */}
+                <div className="flex items-center mb-4">
+                  <img
+                    src={
+                      getAuthors().find((author) => author.id === blog.authorId)
+                        ?.image
+                    }
+                    alt={
+                      getAuthors().find((author) => author.id === blog.authorId)
+                        ?.Name
+                    }
+                    className="w-8 h-8 rounded-full mr-2"
+                  />
+                  <p className="text-sm text-gray-500">
+                    {
+                      getAuthors().find((author) => author.id === blog.authorId)
+                        ?.Name
+                    }{" "}
+                    | {blog.createdDate}
+                  </p>
+                </div>
               </Link>
             </div>
           ))}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-gray-800 py-4 text-center">
-      <div className="container mx-auto">
-        <p>&copy; 2023 Your Website Name. All rights reserved.</p>
+    <footer className="text-gray-800 py-4 text-center border-t border-gray-200 mt-5 mb-5">
+      <div className="container mx-auto text-center">
+        <p>Copyright © 2023 Stablo. All rights reserved.</p>
       </div>
     </footer>
   );
